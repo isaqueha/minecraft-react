a1247d15c17b0568d4430e8a46d3f831ece56f0d
Isaque Hoffmeister <isaque.ha@gmail.com>
2021-01-26 15:14:54 -0300

    feat: cube with texture

---
+++ src/components/Cube.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useBox } from 'use-cannon';
+import * as textures from '../textures';
+
+export const Cube = ({ position, type, ...props }) => {
+  const [ref] = useBox(() => ({
+    type: 'Static',
+    position,
+    ...props,
+  }));
+
+  return (
+    <mesh castShadow ref={ref}>
+      {[...Array(6)].map((_, index) => (
+        <meshStandardMaterial
+          attachArray="material"
+          map={textures[type]}
+          key={index}
+        />
+      ))}
+      <boxBufferGeometry attach="geometry" />
+    </mesh>
+  );
+};
